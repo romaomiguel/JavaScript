602abb0ff0a5d39b903d09bba83a422f769cc29d
romaomiguel <153843652+romaomiguel@users.noreply.github.com>
2024-03-20 15:53:17 -0400

    desafio

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+if (vel > 60) {
+    console.log(`Voce ultrapassou a elocidade permitida. MULTADO`)
+}
+console.log('Dirija sempre usando cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'Brasil'
+if (pais == 'Brasil'){
+    console.log('Voce é Brasileiro')
+}else {
+    console.log('Voce é Estrangeiro')
+}
